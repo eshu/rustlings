1987d74a30f6734ecf8534b116fe00ace3ea8921
Eshu <tjano.xibalba@gmail.com>
2022-07-25 21:23:20 +0900

    Rustlings 5

---
+++ exercises/quiz3.rs
@@ -0,0 +1,80 @@
+// quiz3.rs
+// This quiz tests:
+// - Generics
+// - Traits
+// An imaginary magical school has a new report card generation system written in Rust!
+// Currently the system only supports creating report cards where the student's grade
+// is represented numerically (e.g. 1.0 -> 5.5).
+// However, the school also issues alphabetical grades (A+ -> F-) and needs
+// to be able to print both types of report card!
+
+// Make the necessary code changes in the struct ReportCard and the impl block
+// to support alphabetical report cards. Change the Grade in the second test to "A+"
+// to show that your changes allow alphabetical grades.
+
+// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
+
+
+use std::fmt;
+
+pub struct ReportCard<T> {
+    pub grade: T,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl fmt::Display for ReportCard<f32> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
+    }
+}
+
+impl fmt::Display for ReportCard<&str> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
+    }
+}
+
+impl ReportCard<f32> {
+    pub fn print(&self) -> String {
+        format!("{}", &self)
+    }
+}
+
+impl ReportCard<&str> {
+    pub fn print(&self) -> String {
+        format!("{}", &self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generate_numeric_report_card() {
+        let report_card = ReportCard {
+            grade: 2.1,
+            student_name: "Tom Wriggle".to_string(),
+            student_age: 12,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Tom Wriggle (12) - achieved a grade of 2.1"
+        );
+    }
+
+    #[test]
+    fn generate_alphabetic_report_card() {
+        // TODO: Make sure to change the grade here after you finish the exercise.
+        let report_card = ReportCard {
+            grade: "A+",
+            student_name: "Gary Plotter".to_string(),
+            student_age: 11,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Gary Plotter (11) - achieved a grade of A+"
+        );
+    }
+}
